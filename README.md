b16ffb3df167d0aee35a6e69691cc91909887d4c
Pavel Palancica <pavel.palancica@gmail.com>
2020-04-24 23:48:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SecretFolders-iOS-App
---
+++ SecretFolders-iOS-App/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  SecretFolders-iOS-App
+//
+//  Created by Pavel Palancica on 4/24/20.
+//  Copyright © 2020 I Dev TV. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
